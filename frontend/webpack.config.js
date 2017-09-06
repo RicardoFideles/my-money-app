1964e0e25dd3a64d5225e347a428fd5a75d50a19
Ricardo Fideles <ricardo@ricardofideles.com.br>
2017-09-06 18:04:26 -0300

    forcing versions

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,47 +1,44 @@
 const webpack = require('webpack');
-const ExtractPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry : './src/index.jsx',
-    output : {
+    entry: './src/index.jsx',
+    output: {
         path : __dirname + '/public',
         filename : './app.js'
     },
     devServer : {
-        port : 8080,
+        port: 8080,
         contentBase : './public'
     },
     resolve : {
         extensions : ['', '.js', '.jsx'],
         alias : {
-            modules : __dirname + '/node_modules',
-            jquery : 'modules/admin-lte/bootstrap/js/jQuery-2.2.3.min.js',
-            bootstrap : 'modules/admin-lte/bootstrap/js/bootstrap.js'
+            modules : __dirname + '/node_modules'
         }
     },
     plugins : [
         new webpack.ProvidePlugin({
-            $ : 'jquery',
-            jQuery : 'jquery',
+            $: 'jquery',
+            jQuery: 'jquery',
             'window.jQuery' : 'jquery'
         }),
         new ExtractTextPlugin('app.css')
     ],
     module : {
         loaders : [{
-            test : /.js[x]?$/,
+            test : /\.(js|jsx)?$/,
             loader : 'babel-loader',
             exclude : /node_modules/,
             query : {
                 presets : ['es2015', 'react'],
-                plugins : ['trasform-object-rest-spread']
+                plugins : ['transform-object-rest-spread']
             }
-        }, {
-            text : /\.css$/,
+        },{
+            test : /\.css$/,
             loader : ExtractTextPlugin.extract('style-loader', 'css-loader')
-
         }, {
-            text : /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            test: /\.(svg|woff|woff2|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/,
             loader : 'file'
         }]
     }
